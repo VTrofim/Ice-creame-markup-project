66e84ffe0be858d9c78a6b63c1b3d940bc4a036e
Yevhen <73534149+Krenichniy@users.noreply.github.com>
2023-07-12 08:44:39 +0300

    added products modal

---
+++ src/js/modals.js
@@ -0,0 +1,28 @@
+(() => {
+    const refs = {
+        modal: document.querySelector('[data-modal]'),
+        close: document.querySelector('[data-modal-close]'),
+        open: document.querySelector('[data-modal-open]'),
+    }
+    refs.close.addEventListener('click', toggleModal);
+    refs.open.addEventListener('click', toggleModal);
+
+    function toggleModal(event) {
+        event.preventDefault();
+        refs.modal.classList.toggle('hidden');
+    }
+})();
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
